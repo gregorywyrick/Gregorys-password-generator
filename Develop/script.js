4af5c800b22f453f9c9f6594bee2f68f31615fb5
gregorywyrick <gregmwyrick@gmail.com>
2022-08-31 19:15:08 -0500

    added pop up prompts for user

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,40 @@
 // Assignment code here
+var lettersLower = "abcdefghijklmnopqrstuvwxyz";
+var lettersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var symbols = "!@#$%^&*()_+~\\'|}{[]:;?><,./-=";
 
+var characterSelect = "";
 
+function generatePassword() {
+  var characterLength = prompt ("Choose a number between 8 & 128");
+  console.log(characterLength);
+  if (characterLength < 8 || characterLength > 128) {
+    window.alert( "Please choose a number within the range")
+    return;
+  }
+  var symbolSelect = confirm ("Would you like to include symbols?");
+  console.log(symbolSelect);
+  if (symbolSelect === true) {
+    characterSelect += symbols;
+  }
+  var lowerSelect = confirm ("Would you like to include lowercase letters?")
+  console.log(lowerSelect);
+  if (lowerSelect === true) {
+    characterSelect += lettersLower;
+  }
+  var upperSelect = confirm ("Would you like to include uppercase letters?")
+  console.log(upperSelect);
+  if (upperSelect === true) {
+    characterSelect += lettersUpper;
+  }
+  var numSelect = confirm ("Would you like to include numbers?")
+  console.log(numSelect);
+  if (numSelect === true) {
+    characterSelect += numbers;
+  }
+  console.log(characterSelect);
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
